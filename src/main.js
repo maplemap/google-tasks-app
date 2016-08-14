c2164420627e4d685a252591bf81de656d4ab5fc
Sergey.Illarionov <easthunch@gmail.com>
2016-08-14 18:09:20 +0300

    add: about page

--- src/main.js
+++ src/main.js
@@ -1,23 +1,51 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import injectTapEventPlugin from 'react-tap-event-plugin';
-injectTapEventPlugin();
-import {Router, Route, browserHistory} from "react-router";
+import {Router, Route, Redirect, hashHistory} from "react-router";
+import SessionActions from './actions/SessionActions';
+import SessionStore from './stores/SessionStore';
 
 import './styles/main.less';
 
 import App from './App.jsx';
-import Page from './components/Page.jsx';
+import LoginPage from './components/LoginPage.jsx';
+import LoggedInLayout from './components/LoggedInLayout.jsx';
+import AboutPage from './components/AboutPage.jsx';
+import TasklistsPage from './components/TasklistsPage.jsx';
+import TasksPage from './components/TasksPage.jsx';
 
 if (module.hot) {
     module.hot.accept();
 }
 
-ReactDOM.render(
-    <Router history={browserHistory}>
-      <Route path="/" component={App}>
-        <Route path="/page" component={Page}/>
-      </Route>
-    </Router>,
-    document.getElementById("main")
-);
+window.handleGoogleApiLoaded = () => {
+    SessionActions.authorize(true, renderApp);
+}
+
+
+function renderApp() {
+    ReactDOM.render(
+        <Router history={hashHistory}>
+        <Redirect from="/" to="/login" />
+          <Route path="/" component={App}>
+              <Route path="/login" component={LoginPage} />
+              <Route component={LoggedInLayout} onEnter={requireAuth}>
+                <Route path="/about" component={AboutPage} />
+                <Route path='/lists' component={TasklistsPage}>
+                    <Route path='/lists/:id' component={TasksPage} />
+                </Route>
+              </Route>
+          </Route>
+        </Router>,
+        document.getElementById("main")
+    )
+}
+
+
+function requireAuth(nextState, replace) {
+    if(!SessionStore.isLoggedIn()) {
+        replace({
+            pathname: '/login',
+            state: {nextPathname: nextState.location.pathname}
+        })
+    }
+}
