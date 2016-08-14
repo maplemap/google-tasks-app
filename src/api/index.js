4c327ad3e522cfb62ac7ffbf55efaa0fb7449b88
Sergey.Illarionov <easthunch@gmail.com>
2016-08-14 18:07:59 +0300

    add: api config

---
+++ src/api/index.js
@@ -0,0 +1,24 @@
+const CLIENT_ID = '493022954051-0jar5jadmigsuikpv9qnt1fevjd5661d.apps.googleusercontent.com';
+const SCOPES = ['https://www.googleapis.com/auth/tasks', 'https://www.googleapis.com/auth/plus.me'];
+
+export default {
+    authorize(params) {
+        return new Promise((resolve, reject) => {
+            gapi.auth.authorize(
+                {
+                    'client_id': CLIENT_ID,
+                    'scope': SCOPES,
+                    'immediate': params.immediate,
+                    'cookie_policy': 'single_host_origin'
+                },
+                authResult => {
+                    if(authResult.error) {
+                        return reject(authResult.error);
+                    }
+
+                    return gapi.client.load('tasks', 'v1', () => gapi.client.load('plus', 'v1', () => resolve() ))
+                }
+            );
+        });
+    }
+}
