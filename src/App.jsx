dfc0136dbad257fc010592bb562589f4be01ea3d
Sergey.Illarionov <easthunch@gmail.com>
2016-08-14 15:12:00 +0300

    add: main structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import React from "react";
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
 import './App.less';
+
 
 class App extends React.Component {
     render(){
         return(
-            <div className="App">
-              asdasd
-              <div className="content">
-                {this.props.children}
+            <MuiThemeProvider>
+              <div className="App">
+                <div className="content">
+                  {this.props.children}
+                </div>
               </div>
-            </div>
+            </MuiThemeProvider>
         )
     }
 }
