9c9b4b43d5e66871c0a3bb09c6c3ac95b03482a7
Sergey.Illarionov <easthunch@gmail.com>
2016-08-21 17:39:50 +0300

    update: api

--- src/api/index.js
+++ src/api/index.js
@@ -20,5 +20,57 @@ export default {
                 }
             );
         });
+    },
+
+    listTasksList() {
+        const request = gapi.client.tasks.tasklists.list();
+
+        return new Promise((resolve, reject) => {
+            request.execute(resp => resolve(resp));
+        });
+    },
+
+    inserTaskList({title}) {
+        const request = gapi.client.tasks.tasklists.insert({
+            title: title
+        });
+
+        return new Promise((resolve, reject) => {
+            request.execute(resp => resolve(resp))
+        })
+    },
+
+    listTasks(taskListID) {
+        const request = gapi.client.tasks.tasks.list({
+            tasklist: taskListID
+        });
+
+        return new Promise((resolve, reject) => {
+            request.execute(resp => resolve(resp))
+        })
+    },
+
+    insertTask({taskListID, title}) {
+        const request = gapi.client.tasks.tasks.insert({
+            tasklist: taskListID,
+            title: title
+        });
+
+        return new Promise((resolve, reject) => {
+            request.execute(resp => resolve(resp))
+        })
+    },
+
+    updateTask({ taskListID, taskID, ...params }) {
+        const request = gapi.client.tasks.tasks.update({
+            tasklist : taskListID,
+            task     : taskID,
+            id       : taskID,
+            ...params
+        });
+
+        return new Promise((resolve, reject) => {
+            request.execute(resp => resolve(resp));
+        });
     }
 }
