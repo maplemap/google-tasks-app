a631461c81b0294764d6473a1efd99d8d2c9e5fb
Sergey.Illarionov <easthunch@gmail.com>
2016-12-12 17:32:20 +0200

    refactoring

--- src/components/TasksPage.jsx
+++ src/components/TasksPage.jsx
@@ -1,87 +1,21 @@
 import React from 'react';
 
-import TasksActions from '../actions/TasksActions';
-import TasksStore from '../stores/TasksStore';
-
 import IconButton from 'material-ui/IconButton';
 import ContentAdd from 'material-ui/svg-icons/content/add';
 
 import Task from './Task.jsx';
-import TaskCreateModal from './TaskCreateModal.jsx';
-
 import './TasksPage.less';
 
-function getStateFromFlux() {
-    return {
-        tasks: TasksStore.getTasks()
-    };
-}
+
 
 class TasksPage extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            tasks: getStateFromFlux().tasks,
-            isCreatingTask: false
-        };
-
-        TasksActions.loadTasks(this.props.params.id);
-    }
-
-    componentDidMount() {
-        TasksStore.addChangeListener(this._onChange);
-    }
-
-    componentWillReceiveProps(nextProps) {
-        if (this.props.params.id !== nextProps.params.id) {
-            TasksActions.loadTasks(nextProps.params.id);
-        }
-    }
-
-    componentWillUnmount() {
-        TasksStore.removeChangeListener(this._onChange);
-    }
-
-    handleStatusChange = (taskID, { isCompleted }) => {
-        TasksActions.updateTaskStatus({
-            taskListID: this.props.params.id,
-            taskID: taskID,
-            isCompleted: isCompleted
-        });
-    }
-
-    handleTaskUpdate = (taskID, { text }) => {
-        TasksActions.updateTask({
-            taskListID: this.props.params.id,
-            taskID: taskID,
-            text: text
-        });
-    }
-
-    handleAddTask = () => {
-        this.setState({ isCreatingTask : true });
-    }
-
-    handleClose = () => {
-        this.setState({ isCreatingTask : false });
-    }
-
-    handleTaskSubmit = (task) => {
-        const taskListID = this.props.params.id;
-
-        TasksActions.createTask({ taskListID, ...task });
-
-        this.setState({ isCreatingTask : false });
-    }
-
     render() {
         return (
             <div className='TasksPage'>
                 <div className='TasksPage__header'>
                     <h2 className='TasksPage__title'>List name</h2>
                     <div className='TasksPage__tools'>
-                        <IconButton onClick={this.handleAddTask}>
+                        <IconButton onClick={this.props.onAddTask}>
                             <ContentAdd />
                         </IconButton>
                     </div>
@@ -89,28 +23,19 @@ class TasksPage extends React.Component {
 
                 <div className='TasksPage__tasks'>
                     {
-                        this.state.tasks.map(task =>
+                        this.props.tasks.map(task =>
                             <Task
                                 key={task.id}
                                 text={task.text}
                                 isCompleted={task.isCompleted}
-                                onStatusChange={this.handleStatusChange.bind(null, task.id)}
-                                onUpdate={this.handleTaskUpdate.bind(null, task.id)}
+                                onStatusChange={this.props.onTaskStatusChange.bind(null, task.id)}
+                                onUpdate={this.props.onTaskUpdate.bind(null, task.id)}
                             />
                         )
                     }
                 </div>
-                <TaskCreateModal
-                    isOpen={this.state.isCreatingTask}
-                    onSubmit={this.handleTaskSubmit}
-                    onClose={this.handleClose}
-                />
             </div>
         );
-    }
-
-    _onChange = () => {
-        this.setState(getStateFromFlux());
     }
 };
 
---
+++ src/containers/TasksPage.jsx
@@ -0,0 +1,104 @@
+import React from 'react';
+
+import TasksActions from '../actions/TasksActions';
+import TasksStore from '../stores/TasksStore';
+
+import IconButton from 'material-ui/IconButton';
+import ContentAdd from 'material-ui/svg-icons/content/add';
+
+import TasksPage from '../components/TasksPage.jsx';
+import TaskCreateModal from '../components/TaskCreateModal.jsx';
+
+
+function getStateFromFlux() {
+    return {
+        tasks: TasksStore.getTasks()
+    };
+}
+
+class TasksPageContainer extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            tasks: getStateFromFlux().tasks,
+            isCreatingTask: false
+        };
+
+        TasksActions.loadTasks(this.props.params.id);
+    }
+
+    componentDidMount() {
+        TasksStore.addChangeListener(this._onChange);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (this.props.params.id !== nextProps.params.id) {
+            TasksActions.loadTasks(nextProps.params.id);
+        }
+    }
+
+    componentWillUnmount() {
+        TasksStore.removeChangeListener(this._onChange);
+    }
+
+    handleTaskStatusChange = (taskID, { isCompleted }) => {
+        TasksActions.updateTaskStatus({
+            taskListID: this.props.params.id,
+            taskID: taskID,
+            isCompleted: isCompleted
+        });
+    }
+
+    handleTaskUpdate = (taskID, { text }) => {
+        TasksActions.updateTask({
+            taskListID: this.props.params.id,
+            taskID: taskID,
+            text: text
+        });
+    }
+
+    handleAddTask = () => {
+        this.setState({ isCreatingTask : true });
+    }
+
+    handleClose = () => {
+        this.setState({ isCreatingTask : false });
+    }
+
+    handleTaskSubmit = (task) => {
+        const taskListID = this.props.params.id;
+
+        TasksActions.createTask({ taskListID, ...task });
+
+        this.setState({ isCreatingTask : false });
+    }
+
+    render() {
+        return (
+            <div>
+                <TasksPage
+                    // taskList={this.state.taskList}
+                    tasks={this.state.tasks}
+                    // onUpdateTaskList={this.handleUpdateTaskList}
+                    onAddTask={this.handleAddTask}
+                    // onDeleteTaskList={this.handleDeleteTaskList}
+                    // onTaskDelete={this.handleTaskDelete}
+                    onTaskStatusChange={this.handleTaskStatusChange}
+                    onTaskUpdate={this.handleTaskUpdate}
+                />
+                <TaskCreateModal
+                    isOpen={this.state.isCreatingTask}
+                    onSubmit={this.handleTaskSubmit}
+                    onClose={this.handleClose}
+                />
+            </div>
+        );
+    }
+
+    _onChange = () => {
+        this.setState(getStateFromFlux());
+    }
+};
+
+export default TasksPageContainer;
